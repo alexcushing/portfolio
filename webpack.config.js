3049c20f18e04f71bda75c88dd4555f959f21b5c
alexcushing <alexander_cushing@student.uml.edu>
2016-12-05 15:10:08 -0500

    updated menu

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var config = {
+    context: path.join(__dirname, 'src'),
+    entry: "./index.js",
+
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/public",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: [ 'style', 'css', 'sass' ]
+              }
+        ],
+    }
+};
+module.exports = config;
