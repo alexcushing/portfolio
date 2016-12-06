8e0ba0f6b740c69538fdb950c2d55d05113b6f44
Alex Cushing <alexander_cushing@student.uml.edu>
2016-12-06 15:22:16 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
   <div>
     <Nav name="index" />
-    <div className="panel panel-default limitWidth homeBox">
+    <div className="panel panel-default limitWidth homeBox projFix">
       <div className="panel-body flex-Outer">
         <div className="introduction">
         <img className="image" src="http://alexcushing.com/public/img/me.jpeg" alt="me"/>
@@ -26,10 +26,10 @@ const Home = () => {
         </div>
         <div className="linksPanel">
           <a className="gitLink" href="https://github.com/alexcushing" target="_blank">
-          <img src="http://ocramius.github.io/presentations/proxy-pattern-in-php/assets/img/gh.svg" alt="" className="icon"/> GitHub
+          <img src="http://ocramius.github.io/presentations/proxy-pattern-in-php/assets/img/gh.svg" alt="" className="icon"/> <span className="word"> GitHub</span>
         </a>
           <a className="linkedInLink" href="https://www.linkedin.com/in/alexandercushing" target="_blank">
-          <img src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_color-512.png" alt="" className="icon"/> LinkedIn
+          <img src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_color-512.png" alt="" className="icon"/> <span className="word"> LinkedIn</span>
         </a>
         </div>
       </div>
